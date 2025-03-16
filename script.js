11773cbe35edbf537259b11a0e1e3bae2ecc6dcf
JahanaviChaturvedi <118982130+JahanaviChaturvedi@users.noreply.github.com>
2025-03-16 11:24:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 //your JS code here. If required.
+const inputs = document.querySelectorAll('.code');
+inputs.forEach((input, index) => {
+  input.addEventListener('input', (e) => {
+    const currentInput = e.target;
+    const nextInput = inputs[index + 1];
+    if (currentInput.value && nextInput) {
+      nextInput.focus();
+    }
+  });
+
+  input.addEventListener('keydown', (e) => {
+    const currentInput = e.target;
+    const prevInput = inputs[index - 1];
+
+    if (e.key === 'Backspace') {
+      if (!currentInput.value && prevInput) {
+        prevInput.focus();
+        prevInput.value = '';
+      }
+    }
+  });
+});
